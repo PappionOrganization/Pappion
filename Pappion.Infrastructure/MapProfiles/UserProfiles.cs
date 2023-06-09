3d350d3fb3192f9e57ca7e1054da3925ae47eb56
kramevissergga <kramevissergga@gmail.com>
2023-06-09 18:28:01 +0300

    Fixed User inserting

---
+++ Pappion.Infrastructure/MapProfiles/UserProfiles.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Pappion.Domain.Entities;
+using Pappion.Infrastructure.Dto;
+
+namespace Pappion.Infrastructure.MapProfiles
+{
+    public class UserProfiles : Profile
+    {
+        public UserProfiles()
+        {
+            CreateMap<User, UserReadDto>();
+            CreateMap<UserReadDto, User>();
+            CreateMap<UserAddDto, User>();
+        }
+    }
+}
